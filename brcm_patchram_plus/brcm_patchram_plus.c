be175e3cebca161dc0cc653e6ff00c367ac8f798
Bill Crossley <billcrossley@gmail.com>
2012-04-10 15:22:52 -0400

    re-do of the bluetooth fix. This should now properly parse the bluetooth MAC found on toro and maguro devices as well.
    Conflicts:
    
    	brcm_patchram_plus/brcm_patchram_plus.c

--- brcm_patchram_plus/brcm_patchram_plus.c
+++ brcm_patchram_plus/brcm_patchram_plus.c
@@ -192,6 +192,11 @@ uchar hci_write_pcm_data_format[] =
 uchar hci_write_i2spcm_interface_param[] =
 	{ 0x01, 0x6d, 0xFC, 0x04, 0x00, 0x00, 0x00, 0x00 };
 
+int parse_bdaddr(char*);
+int parse_bdaddr1(char*);
+int parse_bdaddr2(char*);
+int parse_bdaddr3(char*);
+
 #ifdef SAMSUNG_BLUETOOTH
 char* get_samsung_bluetooth_type()
 {
@@ -327,23 +332,112 @@ parse_baudrate(char *optarg)
 	return(0);
 }
 
+
 int
 parse_bdaddr(char *optarg)
 {
+	if ( strstr( optarg, ":") )
+	{
+		/* address is separated by : characters .. use parse_bdaddr1() */
+		return parse_bdaddr1(optarg);
+	}
+	else if ( strstr( optarg, "-" ) )
+	{
+		/* address is separated by - characters .. use parse_bdaddr3 */
+		return parse_bdaddr3(optarg);
+	}
+	else
+	{
+		/* hope the address doesn't have separaters .. use parse_bdaddr2 */
+		return parse_bdaddr2(optarg);
+	}
+}
+
+	
+
+
+int
+parse_bdaddr1(char *optarg)
+{
 	int bd_addr[6];
 	int i;
+	int matches;
+	
+	matches = sscanf(optarg, "%02X:%02X:%02X:%02X:%02X:%02X",
+			 &bd_addr[5], &bd_addr[4], &bd_addr[3],
+			 &bd_addr[2], &bd_addr[1], &bd_addr[0]);
+	
+	/* only do anything if we successfully parsed the mac address */
+	if ( matches == 6 )
+	{
+		for (i = 0; i < 6; i++) {
+			hci_write_bd_addr[4 + i] = bd_addr[i];
+		}
 
-	sscanf(optarg, "%02X:%02X:%02X:%02X:%02X:%02X",
-		&bd_addr[5], &bd_addr[4], &bd_addr[3],
-		&bd_addr[2], &bd_addr[1], &bd_addr[0]);
-
-	for (i = 0; i < 6; i++) {
-		hci_write_bd_addr[4 + i] = bd_addr[i];
+		bdaddr_flag = 1;
+	}
+	else
+	{
+		fprintf( stderr, "Invalid mac address - ignoring.\n")
 	}
 
-	bdaddr_flag = 1;
+	return(0);
+}
+
+
+int
+parse_bdaddr2(char *optarg)
+{
+	int bd_addr[6];
+	int i;
+	int matches;
+
+	matches = sscanf(optarg, "%02X%02X%02X%02X%02X%02X",
+			 &bd_addr[5], &bd_addr[4], &bd_addr[3],
+			 &bd_addr[2], &bd_addr[1], &bd_addr[0]);
+
+	if ( matches == 6 )
+	{
+		for (i = 0; i < 6; i++) {
+			hci_write_bd_addr[4 + i] = bd_addr[i];
+		}
+
+		bdaddr_flag = 1;
+	}
+	else
+	{
+		fprintf( stderr, "Invalid mac address - ignoring.\n")
+	}
 
 	return(0);
+}
+
+int
+parse_bdaddr3(char* optarg)
+{
+	int bd_addr[6];
+	int i;
+	int matches;
+	
+	matches = sscanf(optarg, "%02X-%02X-%02X-%02X-%02X-%02X",
+			 &bd_addr[5], &bd_addr[4], &bd_addr[3],
+			 &bd_addr[2], &bd_addr[1], &bd_addr[0]);
+
+	if ( matches == 6 )
+	{
+		for (i = 0; i < 6; i++) {
+			hci_write_bd_addr[4 + i] = bd_addr[i];
+		}
+
+		bdaddr_flag = 1;
+	}
+	else
+	{
+		fprintf( stderr, "Invalid mac address - ignoring.\n")
+	}
+
+	return(0);
+	
 }
 
 int
@@ -784,57 +878,121 @@ proc_enable_hci()
 	return;
 }
 
-#ifdef ANDROID
+void
+save_bdaddr( char* bdaddr )
+{
+	struct stat st;
+	int fd;
+	
+	if ( stat("/data/misc/bluetoothd", &st) == 0 )
+	{
+	    fd = open("/data/misc/bluetoothd/address", O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP);
+	    if (fd > 0)
+	    {
+		write(fd, bdaddr, strlen(bdaddr));
+		close(fd);
+	    }
+	}
+}
+
 void
 read_default_bdaddr()
 {
 	int sz;
-	int fd;
-
+	int fd;	
 	char path[PROPERTY_VALUE_MAX];
-
+	char addr_from_ril[PROPERTY_VALUE_MAX];
+	char imei_bt[23];
 	char bdaddr[18];
-	int len = 17;
-	memset(bdaddr, 0, (len + 1) * sizeof(char));
 
+	memset(addr_from_ril, 0, sizeof(addr_from_ril));
+	memset(bdaddr, 0, sizeof(bdaddr));
+	memset(path, 0, sizeof(path));
+	
+/*
+*	We can get BT address from previously saved file
+*/
+	fd = open("/data/misc/bluetoothd/address", O_RDONLY);
+	if (fd > 0)
+	{
+		sz = read(fd, addr_from_ril, 17);
+		printf("Read default bdaddr from /data/misc/bluetoothd/address: %s\n", addr_from_ril);		
+		parse_bdaddr(addr_from_ril);
+		close(fd);
+		return;
+	}
+	
+/*
+ *	We can get BT address from /efs (thanks qbanin)
+ */
+	fd = open("/efs/imei/bt.txt", O_RDONLY);
+	if (fd > 0)
+	{
+		sz = read(fd, imei_bt, 23);
+		strncpy(addr_from_ril, imei_bt+11, 12);
+		addr_from_ril[12] = 0;
+		printf("Read default bdaddr from /efs/imei/bt.txt: %s\n", addr_from_ril);
+		close(fd);
+		parse_bdaddr(addr_from_ril);
+		if ( bdaddr_flag == 1 )
+			save_bdaddr(addr_from_ril);
+		return;
+	}
+
+
+/*
+ *	We can get BT address from ril
+ */
+	property_get("ril.bt_macaddr", addr_from_ril, "");
+	if (addr_from_ril[0] != 0)
+	{
+		printf("Read default bdaddr from ril.bt_macaddr: %s\n", addr_from_ril);
+		parse_bdaddr(addr_from_ril);
+		
+		if ( bdaddr_flag == 1 )
+			save_bdaddr(addr_from_ril);
+		return;
+	}
+/*
+ *	Or we can get BT address from a file
+ */
 	property_get("ro.bt.bdaddr_path", path, "");
-	if (path[0] == 0)
-		return;
-
-	fd = open(path, O_RDONLY);
-	if (fd < 0) {
-		fprintf(stderr, "open(%s) failed: %s (%d)", path, strerror(errno),
+	if (path[0] != 0)
+	{
+		fd = open(path, O_RDONLY);
+		if (fd < 0) {
+		    fprintf(stderr, "open(%s) failed: %s (%d)", path, strerror(errno),
 				errno);
-		return;
-	}
+		    return;
+		}
 
-	sz = read(fd, bdaddr, len);
-	if (sz < 0) {
-		fprintf(stderr, "read(%s) failed: %s (%d)", path, strerror(errno),
+		sz = read(fd, bdaddr, sizeof(bdaddr) - 1);
+		if (sz < 0) {
+		    fprintf(stderr, "read(%s) failed: %s (%d)", path, strerror(errno),
 				errno);
-		close(fd);
-		return;
-	} else if (sz != len) {
-		fprintf(stderr, "read(%s) unexpected size %d", path, sz);
-		close(fd);
-		return;
+		    close(fd);
+		    return;
+		} else if (sz != (sizeof(bdaddr)-1) && sz != 12) {
+			fprintf(stderr, "read(%s) unexpected size %d", path, sz);
+			close(fd);
+			return;
+		}
+		
+		printf("Read default bdaddr from ro.bt.bdaddr_path: %s\n", bdaddr);
+		parse_bdaddr(bdaddr);
+		
+		if (bdaddr_flag == 1)
+			save_bdaddr(bdaddr);
 	}
-
-	if (debug) {
-		printf("Read default bdaddr of %s\n", bdaddr);
-	}
-
-	parse_bdaddr(bdaddr);
 }
-#endif
+
 
 
 int
 main (int argc, char **argv)
 {
-#ifdef ANDROID
+
 	read_default_bdaddr();
-#endif
 
 	if (parse_cmd_line(argc, argv)) {
 		exit(1);
