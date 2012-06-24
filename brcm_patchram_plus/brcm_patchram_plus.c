322b0d414342d3396bfc23720f6e7cf4afaf853a
codeworkx <daniel.hillenbrand@codeworkx.de>
2012-06-24 17:34:35 +0200

    brcm_patchram: added support for samsung bluetooth
    Change-Id: I8005acf91c37b82d0e7178a77f878d4b2601ee1e

--- brcm_patchram_plus/brcm_patchram_plus.c
+++ brcm_patchram_plus/brcm_patchram_plus.c
@@ -192,29 +192,71 @@ uchar hci_write_pcm_data_format[] =
 uchar hci_write_i2spcm_interface_param[] =
 	{ 0x01, 0x6d, 0xFC, 0x04, 0x00, 0x00, 0x00, 0x00 };
 
+#ifdef SAMSUNG_BLUETOOTH
+char* get_samsung_bluetooth_type()
+{
+    char buf[10];
+    int fd = open("/data/.cid.info", O_RDONLY);
+    if (fd < 0) {
+        fprintf(stderr, "couldn't open file /data/.cid.info for reading\n");
+        return NULL;
+    }
+
+    if (read(fd, buf, sizeof(buf)) < 0) {
+        close(fd);
+        return NULL;
+    }
+
+    close(fd);
+
+    if (strncmp(buf, "murata", 6) == 0)
+        return "_murata";
+
+    if (strncmp(buf, "semco", 5) == 0)
+        return "_semco";
+
+    return NULL;
+}
+#endif
+
 int
 parse_patchram(char *optarg)
 {
-	char *p;
+    char *p;
 
-	if (!(p = strrchr(optarg, '.'))) {
-		fprintf(stderr, "file %s not an HCD file\n", optarg);
-		exit(3);
-	}
+    if (!(p = strrchr(optarg, '.'))) {
+        fprintf(stderr, "file %s not an HCD file\n", optarg);
+        exit(3);
+    }
 
-	p++;
+    p++;
 
-	if (strcasecmp("hcd", p) != 0) {
-		fprintf(stderr, "file %s not an HCD file\n", optarg);
-		exit(4);
-	}
+    if (strcasecmp("hcd", p) != 0) {
+        fprintf(stderr, "file %s not an HCD file\n", optarg);
+        exit(4);
+    }
 
-	if ((hcdfile_fd = open(optarg, O_RDONLY)) == -1) {
-		fprintf(stderr, "file %s could not be opened, error %d\n", optarg, errno);
-		exit(5);
-	}
+#ifdef SAMSUNG_BLUETOOTH
+    char optarg2[256];
+    char* type = get_samsung_bluetooth_type();
+    char* fext = ".hcd";
 
-	return(0);
+    if (type != NULL) {
+        memset(optarg2, 0, 256);
+        strncpy(optarg2, optarg, strlen(optarg) - 4);
+        strcpy(optarg2 + strlen(optarg2), type);
+        strcpy(optarg2 + strlen(optarg2), fext);
+        optarg = optarg2;
+        fprintf(stderr, "using %s as hcdfile\n", optarg);
+    }
+#endif
+
+    if ((hcdfile_fd = open(optarg, O_RDONLY)) == -1) {
+        fprintf(stderr, "file %s could not be opened, error %d\n", optarg, errno);
+        exit(5);
+    }
+
+    return(0);
 }
 
 void
